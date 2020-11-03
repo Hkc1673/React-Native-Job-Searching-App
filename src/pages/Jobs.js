f25bdabffa741ee031466fcc3862a1bc7fbb6639
Hkc-1673 <habibkc71@gmail.com>
2020-11-03 22:59:55 +0200

    add TopicItem files

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 
 const Jobs = (props) => {
+
+    const {selectedLanguage} = props.route.params;
+
   return (
     <SafeAreaView>
       <View>
-        <Text>JOBS</Text>
+        <Text>{selectedLanguage}</Text>
       </View>
     </SafeAreaView>
   );
---
+++ src/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet, Dimensions} from "react-native";
+
+export const topicItem = StyleSheet.create ({
+    container: {
+        padding: 12,
+        margin: 7,
+        borderRadius: 6,
+
+    },
+    text: {
+        fontWeight: "bold",
+        color: "white"
+    }
+})
+
+export const introduction = StyleSheet.create({
+    banner: {
+        height: Dimensions.get("window").height/3,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
